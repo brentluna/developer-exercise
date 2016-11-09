5706b4f55fc935e25caaea95ee1fa1f31c523b7c
Brent Luna <brentadamluna@gmail.com>
2016-11-08 19:54:52 -0800

    completed  excercises

--- exercise.rb
+++ exercise.rb
@@ -4,7 +4,17 @@ class Exercise
   # Return a string in which every word in "str" that exceeds 4 characters is replaced with "marklar".
   # If the word being replaced has a capital first letter, it should instead be replaced with "Marklar".
   def self.marklar(str)
-    # TODO: Implement this method
+    arr = str.split(' ')
+    result = arr.map { |el| el.length > 4 ? self.convert_str(el) : el }
+    result.join(' ')
+  end
+
+  def self.convert_str(str)
+    punctuation = ''
+    if /\W/.match(str) 
+      punctuation = str[-1]
+    end 
+    str == str.capitalize ? 'Marklar' + punctuation : 'marklar' + punctuation
   end
 
   # Return the sum of all even numbers in the Fibonacci sequence, up to
@@ -12,7 +22,16 @@ class Exercise
   # eg. the Fibonacci sequence up to 6 terms is (1, 1, 2, 3, 5, 8),
   # and the sum of its even numbers is (2 + 8) = 10
   def self.even_fibonacci(nth)
-    # TODO: Implement this method
+    prev_two = [1, 1]
+    result = 0
+
+    (3..nth).each do |num| 
+      prev_sum = prev_two.reduce(:+)
+      result += prev_sum if prev_sum.even?
+      prev_two = [prev_two.last, prev_sum]
+    end
+
+    result
   end
 
 end
